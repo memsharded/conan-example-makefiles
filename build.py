d3f99cecb63cf6fa846b6810213c6532cc135def
memsharded <james@conan.io>
2018-01-05 01:50:10 +0100

    workin in Linux too

--- build.py
+++ build.py
@@ -1,12 +1,15 @@
 import os
 
-os.system("cd hellolib & conan create . user/testing")
-os.system("cd helloapp & conan create . user/testing")
+os.system("cd hellolib && conan create . user/testing")
+os.system("cd helloapp && conan create . user/testing")
 # Alternative 1: deploy()
 os.system("conan install App/0.1@user/testing")
 os.system(os.sep.join([".", "bin", "app"]))
 os.system("rm -rf bin deploy_manifest.txt")
+
 # Alternative 2: with virtualrunenv (no deploy())
-os.system("conan install App/0.1@user/testing -g virtualrunenv")
-os.system("activate_run & app")
-os.system("rm -rf bin deploy_manifest.txt *.bat *.ps1")
+# $ conan install App/0.1@user/testing -g virtualrunenv")
+# Windows use: 
+# $ activate_run.bat && app
+# Linux use:
+# $ source activate_run.sh && app
